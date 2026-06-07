2f627d2c5aba90117687c96a97ea356699f730f2
Kei Okubo <kokubonet@gmail.com>
2026-06-08 03:54:41 +0900

    actionsお試し

---
+++ world-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ERROR_EXIT () {
+	echo "$1" >&2
+	rm -f /tmp/$$-*
+	exit 1
+}
+
+./hello.sh > /tmp/$$-result
+
+echo "hello" > /tmp/$$-ans
+echo "world" >> /tmp/$$-ans
+
+diff /tmp/$$-result /tmp/$$-ans || ERROR_EXIT "test1-1 で失敗"
+
+echo "全てのテストをクリアしました"
+rm /tmp/$$-*
